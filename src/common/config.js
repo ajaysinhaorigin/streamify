4607d085d0c0ef406569805e3e4b262b64968602
Ajay Sinha <ajaysinhaa09@gmail.com>
2023-10-06 16:50:20 +0530

    did some changes

--- src/common/config.js
+++ src/common/config.js
@@ -1,4 +1,4 @@
-export const YOUTUBE_API_KEY = 'AIzaSyBHwNpfu9TUBcGl47wcKitqC5QRRCfi_dA'
+export const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY
 
 export const YOUTUBE_VIDEOS_API =
   'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key='
